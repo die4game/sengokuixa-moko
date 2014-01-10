bd53a7e0aec8051412f5b07d7fe6089b90f5b9e6
die4game <die4game.ult@gmail.com>
2014-01-10 19:24:26 +0900

    getWorld.js実行の条件修正。crxリビルド。

--- MokoLight/contentScripts/getWorld.js
+++ MokoLight/contentScripts/getWorld.js
@@ -1,7 +1,10 @@
-var world = location.host.replace( /^(.+?)\..+/, '$1') + 'ワールド';
-chrome.storage.local.get( world, function ( item) {
-  if ( !item[ world]) {
-    item[world] = {};
-    chrome.storage.local.set( item);
-  }
-});
+var world = location.host.match( /^\w\d\{3}/);
+if ( world) {
+  world = world + 'ワールド';
+  chrome.storage.local.get( world, function ( item) {
+    if ( !item[ world]) {
+      item[world] = {};
+      chrome.storage.local.set( item);
+    }
+  });
+}
